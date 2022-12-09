c5017344f9a88a7d429ff94dfc18623703af826c
tshifura887 <tshifura887@gmail.com>
2022-12-09 09:32:30 +0200

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
-import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter as Router, Routes, Route} from 'react-router-dom';
+import NavBar from './components/NavBar';
+import Home from './components/pages/Home.js';
+import Services from './components/pages/Services';
+import Products from './components/pages/Products';
+import SignUp from './components/pages/SignUp';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+          <NavBar/>
+        <Routes>
+          <Route path='/' exact element={<Home/>}/>
+          <Route path='/services' exact element={<Services/>}/>
+          <Route path='/products' exact element={<Products/>}/>
+          <Route path='/sign-up' exact element={<SignUp/>}/>
+        </Routes>
+      </Router>
     </div>
   );
 }
---
+++ src/components/pages/Home.js
@@ -0,0 +1,15 @@
+import '../../App.css'
+import HeroSection from '../HeroSection.js'
+import React from 'react'
+import Cards from '../Cards'
+import Footer from '../Footer'
+
+export default function Home() {
+  return (
+    <>
+      <HeroSection/>
+      <Cards/>
+      <Footer/>
+    </>
+  )
+}
